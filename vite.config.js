09e67abacb39ad75ff3a5dd028dbf7273ecaad8c
Yasemin KOÇ <yaseminkocc37@gmail.com>
2025-02-23 01:50:05 +0300

    configure Vite server to recognize .jsx files as application/javascript

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    mimeTypes: {
+      ".jsx": "application/javascript",
+    },
+  },
 });
 
